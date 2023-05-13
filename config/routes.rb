d06f1a5b65dbe8137731703298b773e9bca58913
Owen Bonertz <owenbonertz@Owens-MacBook-Air.local>
2023-05-13 02:13:59 -0400

    Building the form and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/home'
+  get '/submit_form', to: 'pages#submit_form'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
