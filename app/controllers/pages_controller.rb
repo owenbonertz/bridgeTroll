4116050b2fc7497eb80f10c629eba4bac49145dc
Owen Bonertz <owenbonertz@Owens-MacBook-Air.local>
2023-05-20 01:32:11 -0400

    Began the logic of the password generation process and began styling the home page using CSS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,19 +16,40 @@ class PagesController < ApplicationController
     # If kiki replace all B,s with Ks, all O's with I; if bouba, do the reverse
     # repeat the string if you want to
 
-    start_bday = birthday[0]
-    start_city = city[0]
-    bday[0] = start_city
-    city[0] = start_bday
+    @city.chomp!
 
-    if bday.length > city.length
-      portmanteau = city + bday
+    start_fart = fart[0]
+    start_bruh = bruh[0]
+    fart[0] = start_bruh
+    bruh[0] = start_fart
+
+    if fart > bruh
+        portmanteau = bruh + fart
     else
-      portmanteau = bday + city
-    # Do something with the form data...
-    
-    # Redirect to the home page
-    redirect_to root_path
+        portmanteau = fart + bruh
+    end
+
+    if gorpy == "kiki"
+        if (portmanteau.include?"k")
+            portmanteau.gsub!("k", "c")
+        end
+        if portmanteau.include?"i"
+            portmanteau.gsub!("i", "e")
+        end
+        
+    elsif gorpy == "booba"
+        puts("bruh")
+        if (portmanteau.include?"b")
+            portmanteau.gsub!("b", "d")
+        end
+        if (portmanteau.include?"o")
+            portmanteau.gsub!("o", "u")
+        end
+        
+    end
+
+    #this code is giving us the proper portmanteau, now we reverse and tack/ delete as many characters as are needed to get to the correct character count
   end
+ 
 
 end
